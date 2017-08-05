a7d9ad6d198a582de077c9437e700519270ac2cf
Gaukas Wang <gaukaswang@gmail.com>
2017-08-05 19:06:11 +0800

    Minor updates
    Use PDO in Capsule, more flexible one.

--- includes/hooks/hook_LoginEmail.php
+++ includes/hooks/hook_LoginEmail.php
@@ -1,40 +1,39 @@
 <?php
 /**
  * @author Tomas <mail@cheuknang.cn>
- * @link https://www.zntec.cn/
- * @version 1.0.0
+ * @updated Gaukas <gaukaswang@gmail.com>
+ * @link https://gaukas.wang/
+ * @version 2.0.0
  */
+use Illuminate\Database\Capsule\Manager as Capsule;
 
-// 常量检查，仅允许 WHMCS 访问
+//Security Check
 if(!defined("WHMCS")){
     die("This file cannot be accessed directly");
 }
 
-/**
- * 声明一个别名，稍候我们要使用 Capsule
- * 有关文档：http://docs.whmcs.com/SQL_Helper_Functions
- */
-use Illuminate\Database\Capsule\Manager as Capsule;
-
-// 改为 ClientAreaHomepage 可以打开客户中心首页测试，改为 ClientLogin 将在登陆后执行
 add_hook('ClientLogin', 1, function ($vars){
-    /**
-     * 使用 Capsule 读取 WHMCS 管理员用户名，用于 API，这样就不必你自己输入了
-     * 有关文档：https://laravel.com/docs/4.2/queries
-     */
-    $whmcsadmin = Capsule::table('tbladmins')->select('username')->first(); // 返回一个对象
+    $pdo = Capsule::connection()->getPdo();
+		$pdo->beginTransaction();
 
-    /**
-     * 判断是否管理员登陆，不是管理员登陆我们才发邮件
-     */
-    if ($_SESSION['adminid'] == false) {
-        $command = "sendemail";
-        $values["messagename"] = "Login Prompt";
-        $values["id"] = $vars['userid'];
-        /**
-         * 使用内部 API 发信
-         * 有关文档：http://docs.whmcs.com/API:Send_Email
-         */
-        localAPI($command, $values, $whmcsadmin->username);
+		try {
+		    $stmt = $pdo->query("SELECT username FROM tbladmins");
+				$adminusername = $stmt->fetch(PDO::FETCH_ASSOC);
+		    $pdo->commit();
+		} catch (\Exception $e) {
+		    $result="Got error when trying to get adminusername {$e->getMessage()}";
+		    $pdo->rollBack();
+		}
+    if(!isset($adminusername['username']))
+    {
+        die("Failed. No username in database detected.");
     }
+    else{
+        if ($_SESSION['adminid'] == false) {
+            $command = "sendemail";
+            $values["messagename"] = "Login Prompt";
+            $values["id"] = $vars['userid'];
+            localAPI($command, $values, $adminusername['username']);
+        }
+     }
 });
